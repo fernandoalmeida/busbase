6dc791d69513319ab12d93f93d265b0c66b89827
Fernando Almeida <fernando@fernandoalmeida.net>
2011-04-15 01:10:33 -0300

    Aplicação Rails vazia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParadaDeOnibusWeb::Application.config.secret_token = 'f6fb032e35b2d8c682c565f94e4b56e27b8cc0bbdb7332111bc48aa524ad4969cab4118d0ead5a17d06ac3d546f4a87cea3094b2f11fc5aa8da53246032dadd2'
